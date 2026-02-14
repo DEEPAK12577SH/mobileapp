01a11de6133755eba53193b871281b02a4b5c358
Deepak Sharma <122333910+DEEPAK12577SH@users.noreply.github.com>
2026-02-14 17:12:08 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = "student-progress-v2";
+const CACHE_NAME = "student-progress-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "logo.png"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json",
-        "./icon.png"
-      ])
-    )
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
